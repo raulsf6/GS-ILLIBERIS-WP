5951d5b4b404a0cf0142fc83935fe523069649bc
Raul Sanchez <raulsf66@gmail.com>
2019-09-16 20:04:46 +0200

    Añadido custom CSS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express =  require('express');
-const routes = require('./routes/index');
+const router = require('./routes/index');
 const path = require("path")
 
 const app = express();
@@ -8,6 +8,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
-app.use('/', routes);
+app.use('/', router);
 
 module.exports = app;
